734dfe4e68b154d5d329664af6bb773eca73fb7e
Simeon <simo_ii94@abv.bg>
2026-08-08 21:50:22 +0300

    Add filtering

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import movieService from "../services/movieService.js";
 
+
 const movieController = Router();
 
 movieController.get('/search', async (req, res) => {
 
-    const searchQuery = req.query || '';
-    console.log(searchQuery);
-    const movies = await movieService.getAll();
+    const filter = req.query
+    const movies = await movieService.getAll(filter);
+
     res.render('movies/search', { movies });
 });
 
