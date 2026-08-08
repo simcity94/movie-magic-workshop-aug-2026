a7b5ceab7206a77c00f19a8c7c840aea91c86d8f
Simeon <simo_ii94@abv.bg>
2026-08-08 14:03:00 +0300

    Add about page

--- src/index.js
+++ src/index.js
@@ -13,4 +13,8 @@ app.get('/', (req, res) => {
     res.render('home', { layout: false })
 });
 
+app.get('/about', (req, res) => {
+    res.render('about', { layout: false })
+});
+
 app.listen(5000, () => console.log('Server is running on http://localhost:5000'));
