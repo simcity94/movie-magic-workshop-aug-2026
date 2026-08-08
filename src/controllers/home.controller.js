903d20b842ffab431a38294d8da5c367db74bc9f
Simeon <simo_ii94@abv.bg>
2026-08-08 22:00:57 +0300

    Add dynamic page title

--- src/controllers/home.controller.js
+++ src/controllers/home.controller.js
@@ -6,11 +6,11 @@ const homeController = Router();
 homeController.get('/', async (req, res) => {
     const movies = await movieRepository.getAll();
 
-    res.render('home', { movies });
+    res.render('home', { movies, pageTitle: 'Home' });
 });
 
 homeController.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', { pageTitle: 'About' });
 });
 
 export default homeController;
--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -9,11 +9,11 @@ movieController.get('/search', async (req, res) => {
     const filter = req.query
     const movies = await movieService.getAll(filter);
 
-    res.render('movies/search', { movies, filter });
+    res.render('movies/search', { movies, filter, pageTitle: 'Search Movies' });
 });
 
 movieController.get('/create', (req, res) => {
-    res.render('movies/create');
+    res.render('movies/create', { pageTitle: 'Create Movie'});
 });
 
 movieController.post('/create', async (req, res) => {
@@ -28,7 +28,7 @@ movieController.get('/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getById(movieId);
 
-    res.render('movies/details', { movie });
+    res.render('movies/details', { movie, pageTitle: movie.title });
 });
 
 export default movieController;
